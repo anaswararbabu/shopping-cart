1a019c89db2c9ec8b9de819fc40998448516c9e0
anaswararbabu <74505032+anaswararbabu@users.noreply.github.com>
2026-03-11 22:20:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(fileUpload());
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/'
+}));
 app.use(session({
   secret: "Key",
   resave: false,

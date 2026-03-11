54810c9321c6852b88deca1a976678a9c7783bea
anaswararbabu <74505032+anaswararbabu@users.noreply.github.com>
2026-03-11 13:11:24 +0530

    Update connection.js

--- configuration/connection.js
+++ configuration/connection.js
@@ -1,36 +1,53 @@
-const mongoClient=require('mongodb').MongoClient
-//const mongoClient = require('mongodb').MongoClient;
-const state ={ db:null }
+[1:09 pm, 11/03/2026] Anaswara: const mongoClient = require('mongodb').MongoClient;
+const state = { db: null };
 
-module.exports.connect = function(done){
-    const url = process.env.MONGO_URL;
-    const dbname = 'shopping';
+module.exports.connect = (done) => {
+    const url = process.env.MONGO_URL; // <-- use env variable
+    if (!url) return done(new Error("MongoDB URL not defined!"));
 
-    /*mongoClient.connect(url,(err,data)=>{
-        if(err)
-            return done(err);
-        state.db = data.db(dbname)
-        console.log('Db connected');
-        done();
-    })*/
+    mongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then((client) => {
+            state.db = client.db();
+            console.log("Db connected successfully");
+            done();
+        })
+        .catch((err) => {
+            console.error("Db connection failed", err);
+            done(err);
+        });
+};
 
-    mongoClient.connect(url)
-    .then((data)=>{
-        state.db=data.db(dbname)
-        console.log('Db connected successfully')
-        done()
-    })
-    .catch((err)=>{
-        console.error('Db connection failed')
-        done(err)
-    })
-    
-}
-
-module.exports.get = function(){
-     if(!state.db){
-        console.error('Db not available')
-        return null
+module.exports.get = () => {
+    if (!state.db) {
+        console.error("Db not available");
+        return null;
     }
-    return state.db
-}
+    return state.db;
+};
+[1:10 pm, 11/03/2026] Anaswara: // configuration/connection.js
+const mongoClient = require('mongodb').MongoClient;
+const state = { db: null };
+
+module.exports.connect = (done) => {
+    const url = process.env.MONGO_URL; // <-- use env variable
+    if (!url) return done(new Error("MongoDB URL not defined!"));
+
+    mongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then((client) => {
+            state.db = client.db();
+            console.log("Db connected successfully");
+            done();
+        })
+        .catch((err) => {
+            console.error("Db connection failed", err);
+            done(err);
+        });
+};
+
+module.exports.get = () => {
+    if (!state.db) {
+        console.error("Db not available");
+        return null;
+    }
+    return state.db;
+};

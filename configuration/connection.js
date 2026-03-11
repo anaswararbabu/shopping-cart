d9a59dd56c6110b5942d2f1b515b7174fb38f5bd
anaswararbabu <74505032+anaswararbabu@users.noreply.github.com>
2026-03-11 13:18:37 +0530

    Update connection.js

--- configuration/connection.js
+++ configuration/connection.js
@@ -24,30 +24,3 @@ module.exports.get = () => {
     }
     return state.db;
 };
-[1:10 pm, 11/03/2026] Anaswara: // configuration/connection.js
-const mongoClient = require('mongodb').MongoClient;
-const state = { db: null };
-
-module.exports.connect = (done) => {
-    const url = process.env.MONGO_URL; // <-- use env variable
-    if (!url) return done(new Error("MongoDB URL not defined!"));
-
-    mongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then((client) => {
-            state.db = client.db();
-            console.log("Db connected successfully");
-            done();
-        })
-        .catch((err) => {
-            console.error("Db connection failed", err);
-            done(err);
-        });
-};
-
-module.exports.get = () => {
-    if (!state.db) {
-        console.error("Db not available");
-        return null;
-    }
-    return state.db;
-};
